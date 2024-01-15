07474c523efa791b1594915d3999212315a3f23b
livio <livio5261@gmail.com>
2024-01-15 12:48:49 +0300

    done

--- server/app.py
+++ server/app.py
@@ -14,25 +14,51 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return '<h1>Bakery GET API</h1>'
 
+
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakeries.append(bakery.to_dict())
+    response = make_response(jsonify(bakeries), 200)
+    return response
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    if not bakery:
+        response_body = "<h1>404 Bakery not found</h1>"
+        response = make_response(response_body, 404)
+        return response
+
+    bakery_dict = bakery.to_dict()
+    response = make_response(jsonify(bakery_dict), 200)
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all():
+        baked_goods.append(baked_good.to_dict())
+    response = make_response(jsonify(baked_goods), 200)
+    return response
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    bakedGood_dict = baked_good.to_dict()
+    response = make_response(jsonify(bakedGood_dict), 200)
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
